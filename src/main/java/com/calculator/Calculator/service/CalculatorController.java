c819c803a07da774b3241694ad268aa4b1d6c07b
Burak Çolak <35burakcolak@gmail.com>
2024-11-06 21:38:24 +0300

    toplama uç noktası yazıldı

--- src/main/java/com/calculator/Calculator/service/CalculatorController.java
+++ src/main/java/com/calculator/Calculator/service/CalculatorController.java
@@ -1,10 +1,19 @@
 package com.calculator.Calculator.service;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/calculator")
 public class CalculatorController {
+
+    @GetMapping("/collect")
+    public int collect(@RequestParam int val1,@RequestParam int val2){
+
+        return val1+val2;
+
+    }
 }
