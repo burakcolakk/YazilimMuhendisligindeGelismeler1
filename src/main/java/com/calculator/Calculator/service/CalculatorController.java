8a10f60b6d83feda058d92d9421661a506c3aaf2
Burak Çolak <35burakcolak@gmail.com>
2024-11-06 21:37:28 +0300

    controller sınıfı oluşturuldu

---
+++ src/main/java/com/calculator/Calculator/service/CalculatorController.java
@@ -0,0 +1,10 @@
+package com.calculator.Calculator.service;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/calculator")
+public class CalculatorController {
+}
